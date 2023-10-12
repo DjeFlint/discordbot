a3fd19a665da67c62ebd7563f71f765a48873f5f
DjeFlingit config --global user.name DjeFlintgit config --global user.name DjeFlin <djeflint@gmail.com>
2023-10-12 12:54:50 +0200

    Verandering aan de code

--- discordbot.py
+++ discordbot.py
@@ -1,3 +1,12 @@
-list = ["appel", "banaan", "kiwi", "aardbei"]
+class Cat:
+    species = "Oriental Shorthair"
+    def __init__(self, name, age, color):
+        self.name = name
+        self.age = age
+        self.color = color
 
-print(list[3])
+Odin = Cat("Odin", 3, "Grijs/Beige")
+Ragnar = Cat("Ragnar", 3, "Zwart")
+
+print(Odin.color, Ragnar.age)
+print(Odin.species)
